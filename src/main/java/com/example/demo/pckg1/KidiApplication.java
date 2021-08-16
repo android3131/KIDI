e55f2620446075202ed1e64938e179d2df1bf638
Mutlaq Hijazi <mutlaq.086@gmail.com>
2021-08-16 14:32:29 +0300

    Category, Kidi Application.java

--- src/main/java/com/example/demo/pckg1/KidiApplication.java
+++ src/main/java/com/example/demo/pckg1/KidiApplication.java
@@ -2,10 +2,10 @@ package com.example.demo.pckg1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={MongoAutoConfiguration.class})
 public class KidiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(KidiApplication.class, args);
 	}
