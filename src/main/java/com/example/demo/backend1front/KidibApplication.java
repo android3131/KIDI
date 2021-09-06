332c7e77a2773f885f56e3b4dea27208fb9afaf8
android3131 <orofra31@gmail.com>
2021-09-06 16:30:01 -0400

    added JWT

--- src/main/java/com/example/demo/backend1front/KidibApplication.java
+++ src/main/java/com/example/demo/backend1front/KidibApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo.backend1front;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class KidibApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(KidibApplication.class, args);
-	}
-
-}
+//package com.example.demo.backend1front;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class KidibApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(KidibApplication.class, args);
+//	}
+//
+//}
